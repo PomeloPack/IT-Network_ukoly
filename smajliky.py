0c2987dff3e3e92c794ab5305678094ba960c5a6
Martin <94791875+PomeloPack@users.noreply.github.com>
2021-12-28 00:31:50 +0100

    Add files via upload

---
+++ smajliky.py
@@ -0,0 +1,12 @@
+smajlík = str(input("Zadej smajlíka: "))
+if smajlík == ":)":
+    print("Tvůj smajlík je veselý")
+elif smajlík == ":(":
+    print("Tvůj smajlík je smutný")
+elif smajlík == ":P":
+    print("Tvůj smajlík je filuta")
+elif smajlík == ":*":
+    print("Tvůj smajlík ti posílá pusinku")
+else:
+    print("Tohoto smajlíka neznám")
+input()
