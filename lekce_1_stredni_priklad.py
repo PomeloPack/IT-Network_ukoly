73433dafa4a984718c36869617605eb6626dca09
Martin <94791875+PomeloPack@users.noreply.github.com>
2021-12-31 14:36:35 +0100

    Add files via upload

---
+++ lekce_1_stredni_priklad.py
@@ -0,0 +1,3 @@
+r_kruhu = float(input("Zadejte poloměr kruhu (cm): "))
+o_obvod_kruhu = print("Obvod zadaného kruhu je: ", 2*3.14*r_kruhu, "cm" )
+S_obsah_kruhu = print(("Jeho obsah je: "), 3.14 * (r_kruhu)*2, "cm^2" )
