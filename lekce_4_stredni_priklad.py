97b8b895139dea631e380e388d58868ba8036bb9
Martin <94791875+PomeloPack@users.noreply.github.com>
2022-01-19 11:49:46 +0100

    Add files via upload

---
+++ lekce_4_stredni_priklad.py
@@ -0,0 +1,17 @@
+cislo1 = 10
+cislo2 = 4
+cislo3 = 1
+
+while ( cislo1 >=5):
+    print(cislo1, "zelených lahví stojí na stole a jedna spadne")
+    cislo1 = cislo1 - 1
+while ( cislo2 >=2):
+    print(cislo2, "zelené lahve stojí na stole a jedna spadne")
+    cislo2 = cislo2 - 1
+while (cislo3 == 1):
+    print(cislo3, "zelená láhaev stojí na stole a jedna spadne")
+    cislo3 = cislo3 - 1
+else:
+    pass
+input()
+
